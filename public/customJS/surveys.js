704c32809bde1198739102a42d54303154ac0f9c
dilantha111 <dilantha.prasanjith111@gmail.com>
2017-01-09 16:03:55 +0530

    css file paths and js file paths has been changed. Becuase it didn't worked as expected at the live server. survey.js frontend file has been modified so that now responses can be taken after a submit click is made.

--- public/customJS/surveys.js
+++ public/customJS/surveys.js
@@ -7,12 +7,19 @@ $(document).ready(function () {
         e.preventDefault();
         var metadata = JSON.parse($("#metadata").val());
         var questions = metadata.questions;
-
-
+        var answers = [];
 
         for(var i = 0 ; i < questions.length ; i++){
-            console.log("printing value");
-            console.log($("#"+i).val());
+            var temp = "";
+            if(questions[i].questionType == "RADIO"){
+                var selector = "input[name='"+i+"']:checked";
+                temp = $(selector+"").val();
+            }else{
+                temp = $("#"+i).val();
+            }
+            answers.push({questionName:questions[i].questionName,value:temp});
         }
+
+        console.log(answers);
     });
 });
